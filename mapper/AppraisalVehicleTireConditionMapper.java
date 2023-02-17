58bc1b781f993dfd4faef841864fde130c02b77a
YogeshKumarV <125454507+YogeshKumarV@users.noreply.github.com>
2023-02-17 10:52:48 +0530

    Update AppraisalVehicleTireConditionMapper.java

--- mapper/AppraisalVehicleTireConditionMapper.java
+++ mapper/AppraisalVehicleTireConditionMapper.java
@@ -1,4 +1,5 @@
 package com.factory.appraisal.vehiclesearchapp.mapper;
+// authorName : YogeshKumarV
 
 import com.factory.appraisal.vehiclesearchapp.dto.AppraisalVehicleTireCondition;
 import com.factory.appraisal.vehiclesearchapp.persistence.model.EAppraisalVehicleTireCondition;
